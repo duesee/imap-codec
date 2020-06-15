69e953cd273772e1c0fcb70fcd5322e64ef78c76
Damian Poddebniak <poddebniak@fh-muenster.de>
2020-06-15 18:04:22 +0200

    moved escape + show newline explicitly and print it

--- src/lib.rs
+++ src/lib.rs
@@ -333,11 +333,30 @@ pub mod utils;
 
 #[cfg(test)]
 mod test {
-    use crate::{
-        codec::escape,
-        parse::{command::command, response::response},
-    };
+    use crate::parse::{command::command, response::response};
     use nom::AsBytes;
+
+    fn escape(bytes: &[u8]) -> String {
+        bytes
+            .iter()
+            .map(|byte| match byte {
+                0x00..=0x08 => format!("\\x{:02x}", byte),
+                0x09 => String::from("\\t"),
+                0x0A => String::from("\\n\n"),
+                0x0B => format!("\\x{:02x}", byte),
+                0x0C => format!("\\x{:02x}", byte),
+                0x0D => String::from("\\r"),
+                0x0e..=0x1f => format!("\\x{:02x}", byte),
+                0x20..=0x22 => format!("{}", *byte as char),
+                0x23..=0x5B => format!("{}", *byte as char),
+                0x5C => String::from("\\\\"),
+                0x5D..=0x7E => format!("{}", *byte as char),
+                0x7f => format!("\\x{:02x}", byte),
+                0x80..=0xff => format!("\\x{:02x}", byte),
+            })
+            .collect::<Vec<String>>()
+            .join("")
+    }
 
     #[test]
     fn test_transcript_from_rfc() {
