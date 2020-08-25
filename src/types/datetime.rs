4d2639be3eeead31b9ee90742492e699baa893a4
Damian Poddebniak <poddebniak@fh-muenster.de>
2020-08-25 18:39:04 +0200

    Implemented `Codec` for `NaiveDate`.

--- src/types/datetime.rs
+++ src/types/datetime.rs
@@ -1,9 +1,21 @@
 use crate::codec::Codec;
-use chrono::{DateTime, FixedOffset};
+use chrono::{DateTime, FixedOffset, NaiveDate};
 
 impl Codec for DateTime<FixedOffset> {
     fn serialize(&self) -> Vec<u8> {
-        // "DQUOTE date-day-fixed "-" date-month "-" date-year SP time SP zone DQUOTE"
+        format!("\"{}\"", self.format("%d-%b-%Y %H:%M:%S %z")).into_bytes()
+    }
+
+    fn deserialize(_input: &[u8]) -> Result<(&[u8], Self), Self>
+    where
+        Self: Sized,
+    {
+        unimplemented!()
+    }
+}
+
+impl Codec for NaiveDate {
+    fn serialize(&self) -> Vec<u8> {
         format!("\"{}\"", self.format("%d-%b-%Y %H:%M:%S %z")).into_bytes()
     }
 
