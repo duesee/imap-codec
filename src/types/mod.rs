a3980033ebec32cbdeeaa035a3fada5a511c1fc9
Damian Poddebniak <poddebniak@fh-muenster.de>
2020-08-26 09:03:15 +0200

    Moved code.

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -80,28 +80,6 @@ impl Codec for AuthMechanism {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum SeqNo {
-    Value(u32),
-    Unlimited,
-}
-
-impl Codec for SeqNo {
-    fn serialize(&self) -> Vec<u8> {
-        match self {
-            SeqNo::Value(number) => number.to_string().into_bytes(),
-            SeqNo::Unlimited => b"*".to_vec(),
-        }
-    }
-
-    fn deserialize(_input: &[u8]) -> Result<(&[u8], Self), Self>
-    where
-        Self: Sized,
-    {
-        unimplemented!()
-    }
-}
-
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Sequence {
     Single(SeqNo),
@@ -115,6 +93,28 @@ impl Codec for Sequence {
             Sequence::Range(from, to) => {
                 [&from.serialize(), b":".as_ref(), &to.serialize()].concat()
             }
+        }
+    }
+
+    fn deserialize(_input: &[u8]) -> Result<(&[u8], Self), Self>
+    where
+        Self: Sized,
+    {
+        unimplemented!()
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum SeqNo {
+    Value(u32),
+    Unlimited,
+}
+
+impl Codec for SeqNo {
+    fn serialize(&self) -> Vec<u8> {
+        match self {
+            SeqNo::Value(number) => number.to_string().into_bytes(),
+            SeqNo::Unlimited => b"*".to_vec(),
         }
     }
 
