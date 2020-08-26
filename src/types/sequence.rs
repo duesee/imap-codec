b23131df878ce42b94d5bb44c87a257bbd7926f3
Damian Poddebniak <poddebniak@fh-muenster.de>
2020-08-26 20:05:42 +0200

    Introduced `ToSequence` trait and implemented it for `Sequence`, `Vec<Sequence>`, and `&str`.

--- src/types/sequence.rs
+++ src/types/sequence.rs
@@ -1,4 +1,4 @@
-use crate::codec::Codec;
+use crate::{codec::Codec, parse::sequence::sequence_set};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Sequence {
@@ -43,5 +43,79 @@ impl Codec for SeqNo {
         Self: Sized,
     {
         unimplemented!()
+    }
+}
+
+pub trait ToSequence {
+    fn to_sequence(self) -> Result<Vec<Sequence>, ()>;
+}
+
+impl ToSequence for Sequence {
+    fn to_sequence(self) -> Result<Vec<Sequence>, ()> {
+        Ok(vec![self])
+    }
+}
+
+impl ToSequence for Vec<Sequence> {
+    fn to_sequence(self) -> Result<Vec<Sequence>, ()> {
+        Ok(self)
+    }
+}
+
+impl ToSequence for &str {
+    fn to_sequence(self) -> Result<Vec<Sequence>, ()> {
+        // FIXME: turn incomplete parser to complete?
+        let blocker = format!("{}|", self);
+
+        if let Ok((b"|", sequence)) = sequence_set(blocker.as_bytes()) {
+            Ok(sequence)
+        } else {
+            Err(())
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::types::sequence::{SeqNo, Sequence, ToSequence};
+
+    #[test]
+    fn test_to_sequence() {
+        let tests = [
+            ("1", vec![Sequence::Single(SeqNo::Value(1))]),
+            (
+                "1,2,3",
+                vec![
+                    Sequence::Single(SeqNo::Value(1)),
+                    Sequence::Single(SeqNo::Value(2)),
+                    Sequence::Single(SeqNo::Value(3)),
+                ],
+            ),
+            ("*", vec![Sequence::Single(SeqNo::Unlimited)]),
+            (
+                "1:2",
+                vec![Sequence::Range(SeqNo::Value(1), SeqNo::Value(2))],
+            ),
+            (
+                "1:2,3",
+                vec![
+                    Sequence::Range(SeqNo::Value(1), SeqNo::Value(2)),
+                    Sequence::Single(SeqNo::Value(3)),
+                ],
+            ),
+            (
+                "1:2,3,*",
+                vec![
+                    Sequence::Range(SeqNo::Value(1), SeqNo::Value(2)),
+                    Sequence::Single(SeqNo::Value(3)),
+                    Sequence::Single(SeqNo::Unlimited),
+                ],
+            ),
+        ];
+
+        for (test, expected) in tests.iter() {
+            let got = test.to_sequence().unwrap();
+            assert_eq!(*expected, got);
+        }
     }
 }
