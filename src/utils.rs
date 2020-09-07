091d7aa13d595be5feaef1403e764a8f2942c1e6
Damian Poddebniak <poddebniak@fh-muenster.de>
2020-09-07 15:39:15 +0200

    Commented out `join_or_nil`

--- src/utils.rs
+++ src/utils.rs
@@ -34,16 +34,16 @@ pub(crate) fn join_serializable<I: Codec>(elements: &[I], sep: &[u8]) -> Vec<u8>
         .join(sep)
 }
 
-pub fn join_or_nil<T: std::fmt::Display>(elements: &[T], sep: &str) -> String {
-    if elements.is_empty() {
-        String::from("nil")
-    } else {
-        String::from("(")
-            + &elements
-                .iter()
-                .map(|x| format!("{}", x))
-                .collect::<Vec<String>>()
-                .join(sep)
-            + ")"
-    }
-}
+//fn join_or_nil<T: std::fmt::Display>(elements: &[T], sep: &str) -> String {
+//    if elements.is_empty() {
+//        String::from("nil")
+//    } else {
+//        String::from("(")
+//            + &elements
+//                .iter()
+//                .map(|x| format!("{}", x))
+//                .collect::<Vec<String>>()
+//                .join(sep)
+//            + ")"
+//    }
+//}
