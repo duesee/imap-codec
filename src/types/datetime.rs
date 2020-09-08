4eec0d248813dfb74267d4e1c110ac22114b43bb
Damian Poddebniak <poddebniak@fh-muenster.de>
2020-09-08 13:27:33 +0200

    Fixed serialization of `NaiveDate`

--- src/types/datetime.rs
+++ src/types/datetime.rs
@@ -16,7 +16,7 @@ impl Codec for DateTime<FixedOffset> {
 
 impl Codec for NaiveDate {
     fn serialize(&self) -> Vec<u8> {
-        format!("\"{}\"", self.format("%d-%b-%Y %H:%M:%S %z")).into_bytes()
+        format!("\"{}\"", self.format("%d-%b-%Y")).into_bytes()
     }
 
     fn deserialize(_input: &[u8]) -> Result<(&[u8], Self), Self>
