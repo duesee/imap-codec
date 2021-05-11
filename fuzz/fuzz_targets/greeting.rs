0ac10e2811afaaf51b27d1120fd71bd39b0e1a3a
Damian Poddebniak <poddebniak@fh-muenster.de>
2021-05-11 10:06:30 +0200

    Fix fuzz targets

--- fuzz/fuzz_targets/greeting.rs
+++ fuzz/fuzz_targets/greeting.rs
@@ -1,17 +1,17 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 
-use imap_codec::{codec::Serialize, parse::response::greeting};
+use imap_codec::{codec::Encode, parse::response::greeting};
 
 fuzz_target!(|data: &[u8]| {
-    if let Ok((rem, parsed1)) = greeting(data) {
-        let input = &data[..data.len() - rem.len()];
+    if let Ok((_rem, parsed1)) = greeting(data) {
+        //let input = &data[..data.len() - rem.len()];
 
         //println!("libFuzzer:  {}", String::from_utf8_lossy(input).trim());
         //println!("parsed:     {:?}", parsed1);
 
         let mut input = Vec::with_capacity(data.len() * 2);
-        parsed1.serialize(&mut input).unwrap();
+        parsed1.encode(&mut input).unwrap();
         //println!("serialized: {}", String::from_utf8_lossy(&input).trim());
         let (rem, parsed2) = greeting(&input).unwrap();
         //println!("parsed:     {:?}", parsed2);
