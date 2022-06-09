afdba282af5b5dc54ebc5cda4c00799a46f4f587
Damian Poddebniak <poddebniak@mailbox.org>
2022-06-09 13:58:04 +0200

    Add (disabled) test to debug crashes.

---
+++ imap-types/tests/arbitrary.rs
@@ -0,0 +1,12 @@
+#[cfg(feature = "arbitrary")]
+#[test]
+fn arbitrary() {
+    // use arbitrary::{Arbitrary, Unstructured};
+    // use imap_types::command::Command;
+    //
+    // let data = include_bytes!("../crash.bin");
+    //
+    // let mut u = Unstructured::new(data);
+    //
+    // Command::arbitrary(&mut u).unwrap();
+}
