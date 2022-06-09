4d4c9bcd8cbec13af3faad18c30de2a9fc609598
Damian Poddebniak <poddebniak@mailbox.org>
2022-06-09 16:22:04 +0200

    Recurse in a controlled manner in `SearchKey::arbitrary(...)`.

--- imap-types/src/arbitrary/mod.rs
+++ imap-types/src/arbitrary/mod.rs
@@ -56,50 +56,117 @@ implement_tryfrom_t! { NonEmptyVec<T>, Vec<T> }
 
 impl<'a> Arbitrary<'a> for SearchKey<'a> {
     fn arbitrary(u: &mut Unstructured<'a>) -> arbitrary::Result<Self> {
-        use SearchKey::*;
+        fn make_search_key<'a>(u: &mut Unstructured<'a>) -> arbitrary::Result<SearchKey<'a>> {
+            Ok(match u.int_in_range(0u8..=33)? {
+                0 => SearchKey::SequenceSet(SequenceSet::arbitrary(u)?),
+                1 => SearchKey::All,
+                2 => SearchKey::Answered,
+                3 => SearchKey::Bcc(AString::arbitrary(u)?),
+                4 => SearchKey::Before(MyNaiveDate::arbitrary(u)?),
+                5 => SearchKey::Body(AString::arbitrary(u)?),
+                6 => SearchKey::Cc(AString::arbitrary(u)?),
+                7 => SearchKey::Deleted,
+                8 => SearchKey::Draft,
+                9 => SearchKey::Flagged,
+                10 => SearchKey::From(AString::arbitrary(u)?),
+                11 => SearchKey::Header(AString::arbitrary(u)?, AString::arbitrary(u)?),
+                12 => SearchKey::Keyword(Atom::arbitrary(u)?),
+                13 => SearchKey::Larger(u32::arbitrary(u)?),
+                14 => SearchKey::New,
+                15 => SearchKey::Old,
+                16 => SearchKey::On(MyNaiveDate::arbitrary(u)?),
+                17 => SearchKey::Recent,
+                18 => SearchKey::Seen,
+                19 => SearchKey::SentBefore(MyNaiveDate::arbitrary(u)?),
+                20 => SearchKey::SentOn(MyNaiveDate::arbitrary(u)?),
+                21 => SearchKey::SentSince(MyNaiveDate::arbitrary(u)?),
+                22 => SearchKey::Since(MyNaiveDate::arbitrary(u)?),
+                23 => SearchKey::Smaller(u32::arbitrary(u)?),
+                24 => SearchKey::Subject(AString::arbitrary(u)?),
+                25 => SearchKey::Text(AString::arbitrary(u)?),
+                26 => SearchKey::To(AString::arbitrary(u)?),
+                27 => SearchKey::Uid(SequenceSet::arbitrary(u)?),
+                28 => SearchKey::Unanswered,
+                29 => SearchKey::Undeleted,
+                30 => SearchKey::Undraft,
+                31 => SearchKey::Unflagged,
+                32 => SearchKey::Unkeyword(Atom::arbitrary(u)?),
+                33 => SearchKey::Unseen,
+                _ => unreachable!(),
+            })
+        }
 
-        use crate::sequence::SequenceSet as SequenceSetData;
+        fn make_search_key_rec<'a>(
+            u: &mut Unstructured<'a>,
+            depth: u8,
+        ) -> arbitrary::Result<SearchKey<'a>> {
+            if depth == 0 {
+                return make_search_key(u);
+            }
 
-        Ok(match u.int_in_range(0u8..=36)? {
-            0 => And(NonEmptyVec::try_from(vec![All, All]).unwrap()), // TODO(testing)
-            1 => SequenceSet(SequenceSetData::arbitrary(u)?),
-            2 => All,
-            3 => Answered,
-            4 => Bcc(AString::arbitrary(u)?),
-            5 => Before(MyNaiveDate::arbitrary(u)?),
-            6 => Body(AString::arbitrary(u)?),
-            7 => Cc(AString::arbitrary(u)?),
-            8 => Deleted,
-            9 => Draft,
-            10 => Flagged,
-            11 => From(AString::arbitrary(u)?),
-            12 => Header(AString::arbitrary(u)?, AString::arbitrary(u)?),
-            13 => Keyword(Atom::arbitrary(u)?),
-            14 => Larger(u32::arbitrary(u)?),
-            15 => New,
-            16 => Not(Box::new(All)), // TODO(testing)
-            17 => Old,
-            18 => On(MyNaiveDate::arbitrary(u)?),
-            19 => Or(Box::new(All), Box::new(All)), // TODO(testing)
-            20 => Recent,
-            21 => Seen,
-            22 => SentBefore(MyNaiveDate::arbitrary(u)?),
-            23 => SentOn(MyNaiveDate::arbitrary(u)?),
-            24 => SentSince(MyNaiveDate::arbitrary(u)?),
-            25 => Since(MyNaiveDate::arbitrary(u)?),
-            26 => Smaller(u32::arbitrary(u)?),
-            27 => Subject(AString::arbitrary(u)?),
-            28 => Text(AString::arbitrary(u)?),
-            29 => To(AString::arbitrary(u)?),
-            30 => Uid(SequenceSetData::arbitrary(u)?),
-            31 => Unanswered,
-            32 => Undeleted,
-            33 => Undraft,
-            34 => Unflagged,
-            35 => Unkeyword(Atom::arbitrary(u)?),
-            36 => Unseen,
-            _ => unreachable!(),
-        })
+            Ok(match u.int_in_range(0u8..=36)? {
+                0 => SearchKey::And({
+                    let keys = {
+                        let len = u.arbitrary_len::<SearchKey>()?;
+                        let mut tmp = Vec::with_capacity(len);
+
+                        for _ in 0..len {
+                            tmp.push(make_search_key_rec(u, depth - 1)?);
+                        }
+
+                        tmp
+                    };
+
+                    if keys.len() >= 1 {
+                        NonEmptyVec::try_from(keys).unwrap()
+                    } else {
+                        NonEmptyVec::try_from(vec![make_search_key(u)?]).unwrap()
+                    }
+                }),
+                1 => SearchKey::SequenceSet(SequenceSet::arbitrary(u)?),
+                2 => SearchKey::All,
+                3 => SearchKey::Answered,
+                4 => SearchKey::Bcc(AString::arbitrary(u)?),
+                5 => SearchKey::Before(MyNaiveDate::arbitrary(u)?),
+                6 => SearchKey::Body(AString::arbitrary(u)?),
+                7 => SearchKey::Cc(AString::arbitrary(u)?),
+                8 => SearchKey::Deleted,
+                9 => SearchKey::Draft,
+                10 => SearchKey::Flagged,
+                11 => SearchKey::From(AString::arbitrary(u)?),
+                12 => SearchKey::Header(AString::arbitrary(u)?, AString::arbitrary(u)?),
+                13 => SearchKey::Keyword(Atom::arbitrary(u)?),
+                14 => SearchKey::Larger(u32::arbitrary(u)?),
+                15 => SearchKey::New,
+                16 => SearchKey::Not(Box::new(make_search_key_rec(u, depth - 1)?)),
+                17 => SearchKey::Old,
+                18 => SearchKey::On(MyNaiveDate::arbitrary(u)?),
+                19 => SearchKey::Or(
+                    Box::new(make_search_key_rec(u, depth - 1)?),
+                    Box::new(make_search_key_rec(u, depth - 1)?),
+                ),
+                20 => SearchKey::Recent,
+                21 => SearchKey::Seen,
+                22 => SearchKey::SentBefore(MyNaiveDate::arbitrary(u)?),
+                23 => SearchKey::SentOn(MyNaiveDate::arbitrary(u)?),
+                24 => SearchKey::SentSince(MyNaiveDate::arbitrary(u)?),
+                25 => SearchKey::Since(MyNaiveDate::arbitrary(u)?),
+                26 => SearchKey::Smaller(u32::arbitrary(u)?),
+                27 => SearchKey::Subject(AString::arbitrary(u)?),
+                28 => SearchKey::Text(AString::arbitrary(u)?),
+                29 => SearchKey::To(AString::arbitrary(u)?),
+                30 => SearchKey::Uid(SequenceSet::arbitrary(u)?),
+                31 => SearchKey::Unanswered,
+                32 => SearchKey::Undeleted,
+                33 => SearchKey::Undraft,
+                34 => SearchKey::Unflagged,
+                35 => SearchKey::Unkeyword(Atom::arbitrary(u)?),
+                36 => SearchKey::Unseen,
+                _ => unreachable!(),
+            })
+        }
+
+        make_search_key_rec(u, 10)
     }
 }
 
