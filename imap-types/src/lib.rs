11b1a6a27d47917a29ea20dc066c35b74465ed71
Damian Poddebniak <poddebniak@mailbox.org>
2022-06-16 11:15:19 +0200

    Documentation.

--- imap-types/src/lib.rs
+++ imap-types/src/lib.rs
@@ -1,3 +1,119 @@
+//! # Misuse-resistant Low-Level Types for the IMAP Protocol
+//!
+//! The two main types in imap-types are [Command](api::command::Command) and [Response](api::response::Response) and we use the term "message" to refer to either of them.
+//!
+//! ## Module structure
+//!
+//! The module structure reflects this terminology:
+//! types that are specific to commands are in the [command](api::command) module;
+//! types that are specific to responses are in the [response](api::response) module;
+//! types that are used in both are in the [message](api::message) module.
+//! The [codec](codec) module contains the [Decode](codec::Decode) trait used to serialize messages.
+//! The [core] module contains "string types" -- there should be no need to use them directly.
+//!
+//! ## Simple construction of messages.
+//!
+//! Messages can be created in different ways.
+//! However, what all ways have in common is, that the API does not allow to create invalid ones.
+//!
+//! For example, all command in IMAP (and many responses) are prefixed with a tag.
+//! Although tags are basically strings, they have additional rules, such as, that no whitespace is allowed.
+//! Thus, imap-codec encapsulates tags in the [Tag](api::message::Tag) struct and makes sure that no invalid tag can be created.
+//! This is why [Result](std::result::Result) is often used in associated functions or methods.
+//!
+//! Generally, imap-codec relies a lot on the [From](std::convert::From), [TryFrom](std::convert::TryFrom), [Into](std::convert::Into), and [TryInto](std::convert::TryInto) traits.
+//! Make good use of them.
+//! However, some types are cumbersome to create.
+//! Thus, there are helper methods, such as, [AuthMechanism::other(...)](api::message::AuthMechanism::other).
+//!
+//! ### Example
+//!
+//! ```
+//! use std::convert::TryFrom;
+//!
+//! use imap_types::{
+//!     command::{Command, CommandBody},
+//!     core::Tag,
+//! };
+//!
+//! // # Variant 1
+//! // Create a `Command` with `tag` "A123" and `body` "NOOP".
+//! // (Note: `Command::new()` returns `Result::Err(...)` when the tag is invalid.)
+//! let cmd = Command::new("A123", CommandBody::Noop).unwrap();
+//!
+//! // # Variant 2
+//! // Create a `CommandBody` first and finalize it into
+//! // a `Command` by attaching a tag later.
+//! let cmd = CommandBody::Noop.tag("A123").unwrap();
+//!
+//! // # Variant 3
+//! // Create a `Command` directly.
+//! let cmd = Command {
+//!     tag: Tag::try_from("A123").unwrap(),
+//!     body: CommandBody::Noop,
+//! };
+//! ```
+//!
+//! ## Serialization of messages.
+//!
+//! All messages implement the `Encode` trait.
+//! You can `use imap_types::Encode` and call the `.encode(...)` method to serialize a message into a writer.
+//! Note that IMAP traces are not guaranteed to be UTF-8. Thus, be careful when using things like `std::str::from_utf8(...).unwrap()`.
+//! It should be generally better to not think about IMAP as being UTF-8.
+//! This is also why `Display` is not implemented.
+//! All types implement `Debug`, though.
+//!
+//! ### Example
+//!
+//! ```
+//! use imap_types::{
+//!     codec::Encode,
+//!     command::{Command, CommandBody},
+//! };
+//!
+//! // Create some command.
+//! let cmd = Command::new("A123", CommandBody::login("alice", "password").unwrap()).unwrap();
+//!
+//! // Create something to encode the `cmd` into.
+//! let mut out = Vec::new();
+//!
+//! // Encode the `cmd` into `out`.
+//! cmd.encode(&mut out);
+//!
+//! // Print the command.
+//! // (Note that IMAP traces are not guaranteed to be valid UTF-8.)
+//! println!("{}", std::str::from_utf8(&out).unwrap());
+//! ```
+//!
+//! ## More complex messages.
+//!
+//! ...
+//!
+//! ### Example
+//!
+//! ```
+//! use std::convert::TryFrom;
+//!
+//! use imap_types::{
+//!     command::{
+//!         Command,
+//!         CommandBody,
+//!         SearchKey,
+//!     },
+//!     core::Charset,
+//! };
+//!
+//! let search = {
+//!     let mailbox = Charset::try_from("Archive").unwrap();
+//!
+//!     CommandBody::search(Some(mailbox), SearchKey::All, false)
+//!         .tag("A123")
+//!         .unwrap()
+//! };
+//!
+//! println!("{:?}", search);
+//! ```
+
 #![deny(missing_debug_implementations)]
 
 #[cfg(feature = "arbitrary")]
