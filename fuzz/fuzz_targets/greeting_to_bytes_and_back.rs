08a758b94b7ff9050744a0ef7a1b9156da576bf4
Damian Poddebniak <poddebniak@mailbox.org>
2022-06-29 17:53:46 +0200

    Add `greeting_to_bytes_and_back` fuzz target.

---
+++ fuzz/fuzz_targets/greeting_to_bytes_and_back.rs
@@ -0,0 +1,53 @@
+#![no_main]
+
+// use std::str::from_utf8;
+
+use imap_codec::{
+    response::greeting,
+    types::{
+        codec::Encode,
+        response::{Code, Greeting},
+    },
+};
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|test: Greeting| {
+    if matches!(&test, Greeting { code, .. } if matches!(code, Some(Code::Referral(_)))) {
+        // FIXME(#30)
+        return;
+    }
+
+    if matches!(&test, Greeting { code, .. } if matches!(code, Some(Code::PermanentFlags(_)| Code::Other(_, _))))
+    {
+        // FIXME(#30)
+        return;
+    }
+
+    if let Some(first) = test.text.inner().chars().next() {
+        if first == '[' {
+            // FIXME(#30)
+            return;
+        }
+    }
+
+    // println!("{:?}", test);
+
+    let mut buffer = Vec::new();
+    test.encode(&mut buffer).unwrap();
+
+    // match std::str::from_utf8(&buffer) {
+    //     Ok(str) => println!("{}", str),
+    //     Err(_) => println!("{:?}", buffer),
+    // }
+
+    // println!("{:?}", from_utf8(&buffer));
+
+    let (rem, parsed) = greeting(&buffer).unwrap();
+    assert!(rem.is_empty());
+
+    // println!("{:?}", parsed);
+
+    assert_eq!(test, parsed);
+
+    // println!("{}", str::repeat("-", 120));
+});
