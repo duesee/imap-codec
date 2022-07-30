2f7ab0e2e451dae0edf52f875bda278c4278f175
Damian Poddebniak <poddebniak@mailbox.org>
2022-07-30 21:23:34 +0200

    Fix (and improve) examples.

--- examples/parse_greeting.rs
+++ examples/parse_greeting.rs
@@ -1,37 +1,8 @@
-use std::io::{Read, Result as IoResult, Write};
+use std::io::Write;
 
 use imap_codec::{codec::Decode, response::Greeting};
 
-pub fn read_file(path: &str) -> IoResult<Vec<u8>> {
-    let mut file = std::fs::File::open(path)?;
-    let mut data = Vec::new();
-    file.read_to_end(&mut data)?;
-
-    Ok(data)
-}
-
 fn main() -> std::io::Result<()> {
-    let mut args = std::env::args();
-
-    if let Some(path) = args.nth(1) {
-        let data = read_file(&path).unwrap();
-
-        match Greeting::decode(&data) {
-            Ok((remaining, greeting)) => {
-                println!("{:#?}", greeting);
-
-                if !remaining.is_empty() {
-                    println!("Remaining data in buffer: {:?}", remaining);
-                }
-            }
-            Err(error) => {
-                println!("Error parsing the greeting. Is it correct? ({:?})", error);
-            }
-        }
-
-        return Ok(());
-    }
-
     loop {
         let line = {
             print!("Enter IMAP4REV1 greeting (or \"exit\"): ");
