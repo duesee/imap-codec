d8c383032b87bead08f237074277ffcf7f96ed45
Damian Poddebniak <poddebniak@mailbox.org>
2023-03-03 18:08:20 +0100

    feat: Add `escape_byte_string` to `utils`.

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,27 @@
 use std::borrow::Cow;
 
+pub fn escape_byte_string(bytes: &[u8]) -> String {
+    bytes
+        .iter()
+        .map(|byte| match byte {
+            0x00..=0x08 => format!("\\x{:02x}", byte),
+            0x09 => String::from("\\t"),
+            0x0A => String::from("\\n"),
+            0x0B => format!("\\x{:02x}", byte),
+            0x0C => format!("\\x{:02x}", byte),
+            0x0D => String::from("\\r"),
+            0x0e..=0x1f => format!("\\x{:02x}", byte),
+            0x20..=0x22 => format!("{}", *byte as char),
+            0x23..=0x5B => format!("{}", *byte as char),
+            0x5C => String::from("\\\\"),
+            0x5D..=0x7E => format!("{}", *byte as char),
+            0x7f => format!("\\x{:02x}", byte),
+            0x80..=0xff => format!("\\x{:02x}", byte),
+        })
+        .collect::<Vec<String>>()
+        .join("")
+}
+
 #[allow(unused)]
 pub fn escape_quoted(unescaped: &str) -> Cow<str> {
     let mut escaped = Cow::Borrowed(unescaped);
