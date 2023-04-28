f0f289ef893a7598bc001fc88b73f52ec1aeca50
Damian Poddebniak <poddebniak@mailbox.org>
2023-04-28 12:01:21 +0200

    fix: Fix `mbx_list_flags`.

--- src/rfc3501/flag.rs
+++ src/rfc3501/flag.rs
@@ -1,11 +1,8 @@
 use abnf_core::streaming::SP;
-use imap_types::{
-    core::Atom,
-    message::{Flag, FlagFetch, FlagNameAttribute, FlagPerm},
-};
+use imap_types::message::{Flag, FlagFetch, FlagNameAttribute, FlagPerm};
 use nom::{
     branch::alt,
-    bytes::streaming::{tag, tag_no_case},
+    bytes::streaming::tag,
     character::streaming::char,
     combinator::{map, recognize, value},
     multi::{separated_list0, separated_list1},
@@ -40,16 +37,19 @@ pub fn flag(input: &[u8]) -> IResult<&[u8], Flag> {
 //     map(atom, Flag::Keyword)(input)
 // }
 
-/// `flag-extension = "\" atom`
-///
-/// Future expansion.
-///
-/// Client implementations MUST accept flag-extension flags.
-/// Server implementations MUST NOT generate flag-extension flags
-/// except as defined by future standard or standards-track revisions of this specification.
-pub fn flag_extension(input: &[u8]) -> IResult<&[u8], Atom> {
-    preceded(tag(b"\\"), atom)(input)
-}
+// Note: This was inlined into `mbx_list_flags`.
+// /// ```abnf
+// /// flag-extension = "\" atom
+// /// ```
+// ///
+// /// Future expansion.
+// ///
+// /// Client implementations MUST accept flag-extension flags.
+// /// Server implementations MUST NOT generate flag-extension flags
+// /// except as defined by future standard or standards-track revisions of this specification.
+// pub fn flag_extension(input: &[u8]) -> IResult<&[u8], Atom> {
+//     preceded(tag(b"\\"), atom)(input)
+// }
 
 /// `flag-list = "(" [flag *(SP flag)] ")"`
 pub fn flag_list(input: &[u8]) -> IResult<&[u8], Vec<Flag>> {
@@ -75,15 +75,18 @@ pub fn flag_perm(input: &[u8]) -> IResult<&[u8], FlagPerm> {
     ))(input)
 }
 
-/// `mbx-list-flags = *(mbx-list-oflag SP) mbx-list-sflag *(SP mbx-list-oflag) /
-///                                        mbx-list-oflag *(SP mbx-list-oflag)`
+/// ```abnf
+/// mbx-list-flags = *(mbx-list-oflag SP) mbx-list-sflag *(SP mbx-list-oflag) /
+///                                        mbx-list-oflag *(SP mbx-list-oflag)
+/// ```
 ///
-/// Note: ABNF enforces that sflag is not used more than once.
-///       We parse any flag and check for multiple occurrences of sflag later.
+/// TODO(#155): ABNF enforces that sflag is not used more than once.
+///             We could parse any flag and check for multiple occurrences of sflag later.
 pub fn mbx_list_flags(input: &[u8]) -> IResult<&[u8], Vec<FlagNameAttribute>> {
-    let (remaining, flags) = separated_list1(SP, alt((mbx_list_sflag, mbx_list_oflag)))(input)?;
+    let (remaining, flags) =
+        separated_list1(SP, map(preceded(char('\\'), atom), FlagNameAttribute::from))(input)?;
 
-    // TODO: Do we really want to enforce this?
+    // TODO(#155): Do we really want to enforce this?
     // let sflag_count = flags
     //     .iter()
     //     .filter(|&flag| FlagNameAttribute::is_selectability(flag))
@@ -99,33 +102,41 @@ pub fn mbx_list_flags(input: &[u8]) -> IResult<&[u8], Vec<FlagNameAttribute>> {
     Ok((remaining, flags))
 }
 
-/// `mbx-list-oflag = "\Noinferiors" / flag-extension`
-///
-/// Other flags; multiple possible per LIST response
-pub fn mbx_list_oflag(input: &[u8]) -> IResult<&[u8], FlagNameAttribute> {
-    alt((
-        value(
-            FlagNameAttribute::Noinferiors,
-            tag_no_case(b"\\Noinferiors"),
-        ),
-        map(flag_extension, FlagNameAttribute::Extension),
-    ))(input)
-}
+// Note: This was inlined into `mbx_list_flags`.
+// /// ```abnf
+// /// mbx-list-oflag = "\Noinferiors" / flag-extension
+// /// ```
+// ///
+// /// Other flags; multiple possible per LIST response
+// pub fn mbx_list_oflag(input: &[u8]) -> IResult<&[u8], FlagNameAttribute> {
+//     alt((
+//         value(
+//             FlagNameAttribute::Noinferiors,
+//             tag_no_case(b"\\Noinferiors"),
+//         ),
+//         map(flag_extension, FlagNameAttribute::Extension),
+//     ))(input)
+// }
 
-/// `mbx-list-sflag = "\Noselect" / "\Marked" / "\Unmarked"`
-///
-/// Selectability flags; only one per LIST response
-pub fn mbx_list_sflag(input: &[u8]) -> IResult<&[u8], FlagNameAttribute> {
-    alt((
-        value(FlagNameAttribute::Noselect, tag_no_case(b"\\Noselect")),
-        value(FlagNameAttribute::Marked, tag_no_case(b"\\Marked")),
-        value(FlagNameAttribute::Unmarked, tag_no_case(b"\\Unmarked")),
-    ))(input)
-}
+// Note: This was inlined into `mbx_list_flags`.
+// /// ```abnf
+// /// mbx-list-sflag = "\Noselect" / "\Marked" / "\Unmarked"
+// /// ```
+// ///
+// /// Selectability flags; only one per LIST response
+// pub fn mbx_list_sflag(input: &[u8]) -> IResult<&[u8], FlagNameAttribute> {
+//     alt((
+//         value(FlagNameAttribute::Noselect, tag_no_case(b"\\Noselect")),
+//         value(FlagNameAttribute::Marked, tag_no_case(b"\\Marked")),
+//         value(FlagNameAttribute::Unmarked, tag_no_case(b"\\Unmarked")),
+//     ))(input)
+// }
 
 #[cfg(test)]
 mod tests {
     use std::convert::TryFrom;
+
+    use imap_types::{core::Atom, message::FlagExtension};
 
     use super::*;
 
@@ -147,6 +158,12 @@ mod tests {
     fn test_flag_perm() {
         let tests = [
             ("\\Deleted)", FlagPerm::Flag(Flag::Deleted)),
+            (
+                "\\Deletedx)",
+                FlagPerm::Flag(Flag::Extension(
+                    FlagExtension::try_from(Atom::try_from("Deletedx").unwrap()).unwrap(),
+                )),
+            ),
             ("\\Seen ", FlagPerm::Flag(Flag::Seen)),
             ("\\*)", FlagPerm::AllowNewKeywords),
         ];
@@ -155,6 +172,25 @@ mod tests {
             let (rem, got) = flag_perm(test.as_bytes()).unwrap();
             assert_eq!(rem.len(), 1);
             assert_eq!(expected, got);
+        }
+    }
+
+    #[test]
+    fn test_mbx_list_flags() {
+        let tests = [
+            (
+                "\\Markedm)",
+                vec![FlagNameAttribute::Extension(
+                    Atom::try_from("Markedm").unwrap(),
+                )],
+            ),
+            ("\\Marked)", vec![FlagNameAttribute::Marked]),
+        ];
+
+        for (test, expected) in tests {
+            let (rem, got) = mbx_list_flags(test.as_bytes()).unwrap();
+            assert_eq!(expected, got);
+            assert_eq!(rem.len(), 1);
         }
     }
 }
