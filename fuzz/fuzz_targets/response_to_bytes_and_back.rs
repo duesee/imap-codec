226072841a42379ce00f1e436aece724480944a2
Damian Poddebniak <poddebniak@mailbox.org>
2023-04-29 11:30:46 +0200

    chore: Fix Clippy lints.

--- fuzz/fuzz_targets/response_to_bytes_and_back.rs
+++ fuzz/fuzz_targets/response_to_bytes_and_back.rs
@@ -1,8 +1,6 @@
 #![no_main]
 
 use base64::{engine::general_purpose::STANDARD as _base64, Engine};
-#[cfg(any(feature = "ext_login_referrals", feature = "ext_mailbox_referrals"))]
-use imap_codec::response::Code;
 #[cfg(feature = "debug")]
 use imap_codec::utils::escape_byte_string;
 use imap_codec::{
