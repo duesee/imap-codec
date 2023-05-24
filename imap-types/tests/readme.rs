d459f292d1825e4f7706b28941b06e7300bbb8c3
Damian Poddebniak <poddebniak@mailbox.org>
2023-05-24 17:44:09 +0200

    docs: Update imap-types' `README.md` (with tested code).

---
+++ imap-types/tests/readme.rs
@@ -0,0 +1,44 @@
+use imap_types::{
+    command::{Command, CommandBody},
+    core::Literal,
+};
+#[cfg(feature = "unchecked")]
+use imap_types::{
+    core::{AString, Atom},
+    message::Tag,
+    security::Secret,
+};
+
+#[test]
+fn test_readme() {
+    Command::new("A1", CommandBody::login("alice", "password").unwrap()).unwrap();
+    Command::new(
+        "A1",
+        CommandBody::login("alice\"", b"\xCA\xFE".as_ref()).unwrap(),
+    )
+    .unwrap();
+    Command::new(
+        "A1",
+        CommandBody::login(Literal::try_from("alice").unwrap(), "password").unwrap(),
+    )
+    .unwrap();
+
+    #[cfg(feature = "unchecked")]
+    {
+        let tag = Tag::try_from("A1").unwrap();
+
+        let _ = Command {
+            tag,
+            body: CommandBody::Login {
+                username: AString::from(Atom::unchecked("alice")),
+                password: Secret::new(AString::from(Atom::unchecked("password"))),
+            },
+        };
+    }
+}
+
+#[test]
+#[should_panic]
+fn test_readme_failing() {
+    Command::new("A1", CommandBody::login("alice\x00", "password").unwrap()).unwrap();
+}
