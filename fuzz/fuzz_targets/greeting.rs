0c8440f61b874ebae0e893bffe8c3e0ef9771589
Damian Poddebniak <poddebniak@mailbox.org>
2023-06-14 13:01:32 +0200

    chore: Deduplicate fuzz-target code.

--- fuzz/fuzz_targets/greeting.rs
+++ fuzz/fuzz_targets/greeting.rs
@@ -1,40 +1,9 @@
 #![no_main]
 
-#[cfg(feature = "debug")]
-use imap_codec::utils::escape_byte_string;
-use imap_codec::{
-    codec::{Decode, Encode},
-    response::Greeting,
-};
+use imap_codec::response::Greeting;
+use imap_codec_fuzz::impl_decode_target;
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    #[cfg(feature = "debug")]
-    println!("[!] Input: {}", escape_byte_string(data));
-
-    if let Ok((_rem, parsed1)) = Greeting::decode(data) {
-        #[cfg(feature = "debug")]
-        {
-            let input = &data[..data.len() - _rem.len()];
-            println!("[!] Consumed: {}", escape_byte_string(input));
-            println!("[!] Parsed1: {parsed1:?}");
-        }
-
-        let output = parsed1.encode().dump();
-        #[cfg(feature = "debug")]
-        println!("[!] Serialized: {}", escape_byte_string(&output));
-
-        let (rem, parsed2) = Greeting::decode(&output).unwrap();
-        #[cfg(feature = "debug")]
-        println!("[!] Parsed2: {parsed2:?}");
-        assert!(rem.is_empty());
-
-        assert_eq!(parsed1, parsed2);
-    } else {
-        #[cfg(feature = "debug")]
-        println!("[!] <invalid>");
-    }
-
-    #[cfg(feature = "debug")]
-    println!("\n\n\n");
+    impl_decode_target!(Greeting, data);
 });
--- fuzz/fuzz_targets/greeting_to_bytes_and_back.rs
+++ fuzz/fuzz_targets/greeting_to_bytes_and_back.rs
@@ -1,30 +1,9 @@
 #![no_main]
 
-#[cfg(feature = "debug")]
-use imap_codec::utils::escape_byte_string;
-use imap_codec::{
-    codec::{Decode, Encode},
-    response::Greeting,
-};
+use imap_codec::response::Greeting;
+use imap_codec_fuzz::impl_to_bytes_and_back;
 use libfuzzer_sys::fuzz_target;
 
-fuzz_target!(|test: Greeting| {
-    #[cfg(feature = "debug")]
-    println!("[!] Input: {test:?}");
-
-    let buffer = test.encode().dump();
-
-    #[cfg(feature = "debug")]
-    println!("[!] Serialized: {}", escape_byte_string(&buffer));
-
-    let (rem, parsed) = Greeting::decode(&buffer).unwrap();
-    assert!(rem.is_empty());
-
-    #[cfg(feature = "debug")]
-    println!("[!] Parsed: {parsed:?}");
-
-    assert_eq!(test, parsed);
-
-    #[cfg(feature = "debug")]
-    println!("{}", str::repeat("-", 120));
+fuzz_target!(|input: Greeting| {
+    impl_to_bytes_and_back!(input, Greeting);
 });
