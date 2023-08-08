699431e67233b50e2da11733f7f03053d5d4fb1f
Damian Poddebniak <poddebniak@mailbox.org>
2023-08-08 12:57:01 +0200

    refactor: Make `FlagExtension` merely a technicality.

--- imap-types/src/flag.rs
+++ imap-types/src/flag.rs
@@ -1,87 +1,95 @@
 //! Flag-related types.
 
+use std::fmt::{Display, Formatter};
+
 #[cfg(feature = "arbitrary")]
 use arbitrary::Arbitrary;
 #[cfg(feature = "bounded-static")]
 use bounded_static::ToStatic;
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
-use thiserror::Error;
 
 use crate::core::{Atom, AtomError};
 
 /// There are two types of flags in IMAP4rev1: System and keyword flags.
 ///
 /// A system flag is a flag name that is pre-defined in RFC3501.
-/// All system flags begin with "\" and certain system flags (`\Deleted` and `\Seen`) have special semantics.
-/// Flags that begin with "\" but are not pre-defined system flags, are extension flags.
+/// All system flags begin with "\\" and certain system flags (`\Deleted` and `\Seen`) have special semantics.
+/// Flags that begin with "\\" but are not pre-defined system flags, are extension flags.
 /// Clients MUST accept them and servers MUST NOT send them except when defined by future standard or standards-track revisions.
 ///
 /// A keyword is defined by the server implementation.
-/// Keywords do not begin with "\" and servers may permit the client to define new ones
-/// in the mailbox by sending the "\*" flag ([`FlagPerm::Asterisk`]) in the PERMANENTFLAGS response..
+/// Keywords do not begin with "\\" and servers may permit the client to define new ones
+/// in the mailbox by sending the `\*` flag ([`FlagPerm::Asterisk`]) in the PERMANENTFLAGS response..
 ///
 /// Note that a flag of either type can be permanent or session-only.
-#[cfg_attr(feature = "arbitrary", derive(Arbitrary))]
 #[cfg_attr(feature = "bounded-static", derive(ToStatic))]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum Flag<'a> {
-    /// Message has been read (`\Seen`).
-    Seen,
     /// Message has been answered (`\Answered`).
     Answered,
-    /// Message is "flagged" for urgent/special attention (`\Flagged`).
-    Flagged,
     /// Message is "deleted" for removal by later EXPUNGE (`\Deleted`).
     Deleted,
     /// Message has not completed composition (marked as a draft) (`\Draft`).
     Draft,
+    /// Message is "flagged" for urgent/special attention (`\Flagged`).
+    Flagged,
+    /// Message has been read (`\Seen`).
+    Seen,
     /// A future expansion of a system flag.
     Extension(FlagExtension<'a>),
     /// A keyword.
     Keyword(Atom<'a>),
 }
 
+/// An (extension) flag.
+///
+/// It's guaranteed that this type can't represent any flag from [`Flag`].
+#[cfg_attr(feature = "bounded-static", derive(ToStatic))]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct FlagExtension<'a>(Atom<'a>);
+
 impl<'a> Flag<'a> {
-    pub fn system<A>(value: A) -> Result<Self, FlagError<'a, A::Error>>
-    where
-        A: TryInto<Atom<'a>>,
-    {
-        let atom = value.try_into()?;
-
+    pub fn system(atom: Atom<'a>) -> Self {
         match atom.as_ref().to_ascii_lowercase().as_ref() {
-            "answered" => Ok(Self::Answered),
-            "flagged" => Ok(Self::Flagged),
-            "deleted" => Ok(Self::Deleted),
-            "seen" => Ok(Self::Seen),
-            "draft" => Ok(Self::Draft),
-            _ => Err(FlagError::IsAnExtensionFlag { candidate: atom }),
+            "answered" => Self::Answered,
+            "deleted" => Self::Deleted,
+            "draft" => Self::Draft,
+            "flagged" => Self::Flagged,
+            "seen" => Self::Seen,
+            _ => Self::Extension(FlagExtension(atom)),
         }
     }
 
-    pub fn extension<A>(value: A) -> Result<Self, FlagError<'a, A::Error>>
-    where
-        A: TryInto<Atom<'a>>,
-    {
-        let atom = value.try_into()?;
-
-        match atom.as_ref().to_ascii_lowercase().as_ref() {
-            "answered" | "flagged" | "deleted" | "seen" | "draft" => {
-                Err(FlagError::IsASystemFlag { candidate: atom })
-            }
-            _ => Ok(Self::Extension(FlagExtension(atom))),
-        }
+    pub fn keyword(atom: Atom<'a>) -> Self {
+        Self::Keyword(atom)
     }
+}
 
-    pub fn system_or_extension(atom: Atom<'a>) -> Self {
-        match Self::system(atom) {
-            Ok(system) => system,
-            Err(FlagError::Atom(_)) => unreachable!(),
-            Err(FlagError::IsASystemFlag { .. }) => unreachable!(),
-            Err(FlagError::IsAnExtensionFlag { candidate }) => {
-                Self::Extension(FlagExtension(candidate))
-            }
+impl<'a> TryFrom<&'a str> for Flag<'a> {
+    type Error = AtomError;
+
+    fn try_from(value: &'a str) -> Result<Self, Self::Error> {
+        Ok(if let Some(value) = value.strip_prefix('\\') {
+            Self::system(Atom::try_from(value)?)
+        } else {
+            Self::keyword(Atom::try_from(value)?)
+        })
+    }
+}
+
+impl<'a> Display for Flag<'a> {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        match self {
+            Flag::Answered => f.write_str("\\Answered"),
+            Flag::Deleted => f.write_str("\\Deleted"),
+            Flag::Draft => f.write_str("\\Draft"),
+            Flag::Flagged => f.write_str("\\Flagged"),
+            Flag::Seen => f.write_str("\\Seen"),
+            Flag::Extension(other) => write!(f, "\\{}", other.0),
+            Flag::Keyword(atom) => write!(f, "{}", atom),
         }
     }
 }
@@ -112,43 +120,6 @@ pub enum FlagPerm<'a> {
     /// Indicates that it is possible to create new keywords by
     /// attempting to store those flags in the mailbox (`\*`).
     Asterisk,
-}
-
-/// Client implementations MUST accept flag-extension flags.
-/// Server implementations MUST NOT generate flag-extension flags
-/// except as defined by future standard or standards-track revisions of this specification.
-#[cfg_attr(feature = "bounded-static", derive(ToStatic))]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct FlagExtension<'a>(pub(crate) Atom<'a>);
-
-impl<'a> TryFrom<Atom<'a>> for FlagExtension<'a> {
-    type Error = FlagError<'a, AtomError>;
-
-    fn try_from(value: Atom<'a>) -> Result<Self, Self::Error> {
-        match value.as_ref().to_ascii_lowercase().as_ref() {
-            "answered" | "flagged" | "deleted" | "seen" | "draft" => {
-                Err(FlagError::IsASystemFlag { candidate: value })
-            }
-            _ => Ok(Self(value)),
-        }
-    }
-}
-
-impl<'a> AsRef<str> for FlagExtension<'a> {
-    fn as_ref(&self) -> &str {
-        self.0.as_ref()
-    }
-}
-
-#[derive(Clone, Debug, Eq, Error, Hash, Ord, PartialEq, PartialOrd)]
-pub enum FlagError<'a, A> {
-    #[error(transparent)]
-    Atom(#[from] A),
-    #[error("Is a system flag.")]
-    IsASystemFlag { candidate: Atom<'a> },
-    #[error("Is an extension flag.")]
-    IsAnExtensionFlag { candidate: Atom<'a> },
 }
 
 /// Four name attributes are defined.
