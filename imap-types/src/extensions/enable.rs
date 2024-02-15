9168a7b798a162208745bcde41bb3a29f6425d86
Damian Poddebniak <poddebniak@mailbox.org>
2024-02-15 16:53:53 +0100

    docs: Fix warnings

--- imap-types/src/extensions/enable.rs
+++ imap-types/src/extensions/enable.rs
@@ -3,7 +3,7 @@
 //! This extension extends ...
 //!
 //! * the [Capability](crate::response::Capability) enum with a new variant [Capability::Enable](crate::response::Capability#variant.Enable),
-//! * the [CommandBody](crate::command::CommandBody) enum with a new variant [CommandBody::Enable](crate::command::CommandBody#variant.Enable), and
+//! * the [CommandBody] enum with a new variant [CommandBody::Enable], and
 //! * the [Data](crate::response::Data) enum with a new variant [Data::Enabled](crate::response::Data#variant.Enabled).
 
 use std::fmt::{Display, Formatter};
