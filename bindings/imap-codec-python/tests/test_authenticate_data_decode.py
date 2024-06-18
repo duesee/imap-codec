871ed5647fee85e9b3cf95ae2539ad5857b55b42
Henning Holm <git@henningholm.de>
2024-06-18 21:32:23 +0200

    feat: Add Python bindings for decoders
    Wrap codecs from `imap-codec` into `pyobject` types to be used from
    Python.
    For each of the codecs, the `decode` method is implemented and
    corresponding Python tests are added to prevent regressions on the
    bindings. The encoding implementation will be added in a subsequent
    commit.
    Currently, the resulting `dict`/`tuple` types are still very rough which
    should be changed further down the road.

---
+++ bindings/imap-codec-python/tests/test_authenticate_data_decode.py
@@ -0,0 +1,29 @@
+import unittest
+
+from imap_codec import AuthenticateDataCodec, DecodeFailed, DecodeIncomplete
+
+
+class TestAuthenticateDataDecode(unittest.TestCase):
+    def test_authenticate_data(self):
+        buffer = b"VGVzdA==\r\n<remaining>"
+        remaining, authenticate_data = AuthenticateDataCodec.decode(buffer)
+        self.assertEqual(authenticate_data, {"Continue": list(b"Test")})
+        self.assertEqual(remaining, b"<remaining>")
+
+    def test_authenticate_data_without_remaining(self):
+        buffer = b"VGVzdA==\r\n"
+        remaining, authenticate_data = AuthenticateDataCodec.decode(buffer)
+        self.assertEqual(authenticate_data, {"Continue": list(b"Test")})
+        self.assertEqual(remaining, b"")
+
+    def test_authenticate_data_error_incomplete(self):
+        buffer = b"VGV"
+        with self.assertRaises(DecodeIncomplete) as cm:
+            AuthenticateDataCodec.decode(buffer)
+        self.assertEqual(str(cm.exception), "")
+
+    def test_authenticate_data_error_failed(self):
+        buffer = b"VGVzdA== \r\n"
+        with self.assertRaises(DecodeFailed) as cm:
+            AuthenticateDataCodec.decode(buffer)
+        self.assertEqual(str(cm.exception), "")
---
+++ bindings/imap-codec-python/tests/test_idle_done_decode.py
@@ -0,0 +1,29 @@
+import unittest
+
+from imap_codec import DecodeFailed, DecodeIncomplete, IdleDoneCodec
+
+
+class TestIdleDoneDecode(unittest.TestCase):
+    def test_idle_done(self):
+        buffer = b"done\r\n<remaining>"
+        remaining, idle_done = IdleDoneCodec.decode(buffer)
+        self.assertEqual(idle_done, ())
+        self.assertEqual(remaining, b"<remaining>")
+
+    def test_idle_done_without_remaining(self):
+        buffer = b"done\r\n"
+        remaining, idle_done = IdleDoneCodec.decode(buffer)
+        self.assertEqual(idle_done, ())
+        self.assertEqual(remaining, b"")
+
+    def test_idle_done_error_incomplete(self):
+        buffer = b"do"
+        with self.assertRaises(DecodeIncomplete) as cm:
+            IdleDoneCodec.decode(buffer)
+        self.assertEqual(str(cm.exception), "")
+
+    def test_idle_done_error_failed(self):
+        buffer = b"done \r\n"
+        with self.assertRaises(DecodeFailed) as cm:
+            IdleDoneCodec.decode(buffer)
+        self.assertEqual(str(cm.exception), "")
