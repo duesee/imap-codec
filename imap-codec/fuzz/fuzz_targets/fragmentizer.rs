4310ba29033bc3c0296054515c92289ca428ff8c
Damian Poddebniak <poddebniak@mailbox.org>
2024-07-13 14:20:04 +0200

    review

--- imap-codec/fuzz/fuzz_targets/fragmentizer.rs
+++ imap-codec/fuzz/fuzz_targets/fragmentizer.rs
@@ -3,22 +3,17 @@
 use imap_codec::fragmentizer::{Fragmentizer, MaxMessageSize};
 use libfuzzer_sys::fuzz_target;
 
-fuzz_target!(|input: (MaxMessageSize, Vec<&[u8]>)| {
+fuzz_target!(|input: (MaxMessageSize, Vec<Vec<u8>>)| {
     let (mms, data) = input;
 
     #[cfg(feature = "debug")]
-    println!("input: {mms:?}, {data:?}");
-
-    let mut fragmentizer = Fragmentizer::new(mms);
+    println!("input: mms={mms:?}, data={data:?}");
 
     // Ensure `Fragmentizer` recreates input data (when smaller than `mms`).
     let mut emitted_bytes = Vec::new();
-    let check_recreation = match mms {
-        MaxMessageSize::Unlimited => true,
-        MaxMessageSize::Limited(limit) => {
-            data.iter().map(|s| s.len()).sum::<usize>() <= limit as usize
-        }
-    };
+    let data_flat = data.iter().flatten().copied().collect::<Vec<u8>>();
+
+    let mut fragmentizer = Fragmentizer::new(mms);
 
     for chunk in &data {
         loop {
@@ -27,10 +22,8 @@ fuzz_target!(|input: (MaxMessageSize, Vec<&[u8]>)| {
                     #[cfg(feature = "debug")]
                     println!("{fragment_info:?}");
 
-                    if check_recreation {
-                        // Collect emitted bytes.
-                        emitted_bytes.extend_from_slice(fragmentizer.fragment_bytes(fragment_info));
-                    }
+                    // Collect emitted bytes.
+                    emitted_bytes.extend_from_slice(fragmentizer.fragment_bytes(fragment_info));
                 }
                 None => {
                     fragmentizer.enqueue_bytes(chunk);
@@ -40,9 +33,18 @@ fuzz_target!(|input: (MaxMessageSize, Vec<&[u8]>)| {
         }
     }
 
-    if check_recreation {
+    assert!(emitted_bytes.len() <= data_flat.len());
+
+    let check_prefix = match mms {
+        MaxMessageSize::Unlimited => true,
+        MaxMessageSize::Limited(limit) => data_flat.len() <= limit as usize,
+    };
+
+    if check_prefix {
+        #[cfg(feature = "debug")]
+        println!("Checking prefix...");
+
         // Ensure emitted bytes are prefix of fuzzing input data.
-        let input = data.into_iter().flat_map(Vec::from).collect::<Vec<u8>>();
-        assert_eq!(input[..emitted_bytes.len()], emitted_bytes);
+        assert_eq!(emitted_bytes, data_flat[..emitted_bytes.len()]);
     }
 });
