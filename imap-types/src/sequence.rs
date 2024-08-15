630097e1cef0e62a72839b9c8f60896d03788dce
Damian Poddebniak <poddebniak@mailbox.org>
2024-08-15 20:06:46 +0200

    chore: temporarily ignore #590

--- imap-types/src/sequence.rs
+++ imap-types/src/sequence.rs
@@ -224,6 +224,8 @@ macro_rules! impl_try_from_num {
             type Error = ValidationError;
 
             fn try_from(value: $num) -> Result<Self, Self::Error> {
+                // TODO(590)
+                #[allow(irrefutable_let_patterns)]
                 if let Ok(value) = u32::try_from(value) {
                     if let Ok(value) = NonZeroU32::try_from(value) {
                         return Ok(Self::Value(value));
