378f2a35a07bbc3e16d08cee3f3380ce75ed10a4
Damian Poddebniak <poddebniak@mailbox.org>
2025-05-09 18:56:49 +0200

    chore: remove unused braces

--- imap-codec/src/response.rs
+++ imap-codec/src/response.rs
@@ -792,14 +792,10 @@ mod tests {
         assert!(response_data(b"* STATUS INBOX (MESSAGES 100 UNSEEN 0)  \r\n").is_err());
 
         #[cfg(not(feature = "quirk_trailing_space"))]
-        {
-            assert!(response_data(b"* STATUS INBOX (MESSAGES 100 UNSEEN 0) \r\n").is_err());
-        }
+        assert!(response_data(b"* STATUS INBOX (MESSAGES 100 UNSEEN 0) \r\n").is_err());
 
         #[cfg(feature = "quirk_trailing_space")]
-        {
-            assert!(response_data(b"* STATUS INBOX (MESSAGES 100 UNSEEN 0) \r\n").is_ok());
-        }
+        assert!(response_data(b"* STATUS INBOX (MESSAGES 100 UNSEEN 0) \r\n").is_ok());
     }
 
     #[test]
