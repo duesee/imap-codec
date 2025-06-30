153fb31787e0850a992daa4595229277c8e05063
Damian Poddebniak <poddebniak@mailbox.org>
2025-06-30 13:04:40 +0200

    chore: revert spurious newline

--- imap-types/src/command.rs
+++ imap-types/src/command.rs
@@ -2119,6 +2119,7 @@ mod tests {
 
         for (no, cmd_body) in cmds.into_iter().enumerate() {
             println!("Test: {no}, {cmd_body:?}");
+
             let _ = cmd_body.tag(format!("A{no}")).unwrap();
         }
     }
