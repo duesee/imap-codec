aaed19e92f5ee67a88e274beff162b64bf8d2da7
Damian Poddebniak <poddebniak@mailbox.org>
2025-06-30 20:07:20 +0200

    chore: update flake.nix

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
           # `rust-toolchain.toml` configuration file
           rust-toolchain = fenix.packages.${system}.fromToolchainFile {
             file = ./rust-toolchain.toml;
-            sha256 = "opUgs6ckUQCyDxcB9Wy51pqhd0MPGHUVbwRKKPGiwZU=";
+            sha256 = "Qxt8XAuaUR2OMdKbN4u8dBJOhSHxS+uS06Wl9+flVEk=";
           };
 
         in
