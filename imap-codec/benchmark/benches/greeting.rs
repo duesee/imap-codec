0ea7486600c21ad0fea6f1f185edd0b390e005e1
Damian Poddebniak <poddebniak@mailbox.org>
2026-06-03 23:17:18 +0200

    chore: update deps

--- imap-codec/benchmark/benches/greeting.rs
+++ imap-codec/benchmark/benches/greeting.rs
@@ -1,4 +1,6 @@
-use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use std::hint::black_box;
+
+use criterion::{Criterion, criterion_group, criterion_main};
 use imap_codec::{GreetingCodec, decode::Decoder, encode::Encoder, imap_types::response::Greeting};
 use imap_types::{
     auth::AuthMechanism,
